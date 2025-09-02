7f723aa01bb0542a0e7e932eaabab70afdf8336a
Fabio Felici <fabio.felici@pagopa.it>
2025-09-02 16:35:54 +0200

    Refactor function naming and update folder structure to improve readability and organization

---
+++ src/core/orchestrator/trigger/function_app.py
@@ -0,0 +1,20 @@
+import logging
+
+import azure.functions as func
+from requests import request
+
+app = func.FunctionApp()
+
+
+@app.route(route="Trigger", auth_level=func.AuthLevel.FUNCTION)
+def trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Request parameters: %s", req.params)
+
+    response = request("GET", "http://localhost:8080/api/inbound")
+    response_content = response.content
+    logging.info("Response content: %s", response_content)
+
+    return func.HttpResponse(
+        "Triggered!!",
+        status_code=200,
+    )
