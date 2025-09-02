a86c87f717184fa1017a68ba97c3d1206592d37a
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2025-09-02 16:10:40 +0200

    init terraform module

---
+++ src/core/iac/tests/main.tf
@@ -0,0 +1,49 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.project}-rg"
+  location = var.location
+
+  tags = var.tags
+}
+
+resource "azurerm_app_service_plan" "this" {
+  name                = "${local.project}-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+
+  sku {
+    tier = "Standard"
+    size = "B1"
+  }
+  tags = var.tags
+}
+
+module "storage_account" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v4.git//storage_account?ref=8265f125b07251a5efe7b9ff57707109de8b46ba"
+
+  name                          = "${local.project}sa"
+  location                      = var.location
+  resource_group_name           = azurerm_resource_group.rg.name
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
+  is_hns_enabled                = false
+  public_network_access_enabled = true
+  tags                          = var.tags
+}
+
+module "function_app" {
+  source = "../"
+
+  name                       = "${local.project}-function-app"
+  location                   = var.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.this.id
+  storage_account_name       = module.storage_account.name
+  storage_account_access_key = module.storage_account.primary_access_key
+
+  source_control = {
+    repo_url = "https://github.com/pagopa/unified-runbook-automation-manager"
+    branch   = "main"
+  }
+  kind = "StorageV2"
+}
