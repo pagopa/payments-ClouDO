15b441ec42ebce1d07a7073c96939e4c44ca4a49
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2025-09-18 15:27:06 +0200

    terraform module

---
+++ src/core/iac/data.tf
@@ -0,0 +1,27 @@
+# App Insights
+data "azurerm_application_insights" "this" {
+  name                = var.application_insights_name
+  resource_group_name = var.application_insights_rg
+}
+
+# Function APP Keys
+data "azurerm_function_app_host_keys" "orchestrator" {
+  name                = azurerm_linux_function_app.orchestrator.name
+  resource_group_name = azurerm_linux_function_app.orchestrator.resource_group_name
+
+  depends_on = [azurerm_linux_function_app.orchestrator]
+}
+
+data "azurerm_function_app_host_keys" "worker" {
+  name                = azurerm_linux_function_app.worker.name
+  resource_group_name = azurerm_linux_function_app.worker.resource_group_name
+
+  depends_on = [azurerm_linux_function_app.worker]
+}
+
+# Random UUID for RowKey
+resource "random_uuid" "uuid" {
+  for_each = {
+    for i in local.entity_executor : i.entity.id => i
+  }
+}
--- src/core/iac/tests/versions.tf
+++ src/core/iac/tests/versions.tf
@@ -19,11 +19,3 @@ provider "azurerm" {
     }
   }
 }
-
-resource "random_id" "unique" {
-  byte_length = 3
-}
-
-locals {
-  project = "${var.prefix}${random_id.unique.hex}"
-}
