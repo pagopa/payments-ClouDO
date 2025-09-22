371c722022949452677dc9c4fd5d07d2eccbf559
Fabio Felici <fabio.felici@pagopa.it>
2025-09-22 19:27:08 +0200

    Improve managed identity login logic in `aztest.sh`
    - Added support for specifying client ID in managed identity login.
    - Optional subscription ID handling to enhance flexibility.
    - Refined error checks and messaging for better clarity.

--- src/runbooks/aztest.sh
+++ src/runbooks/aztest.sh
@@ -2,16 +2,32 @@
 
 # Log in using managed identity
 echo "Logging in with managed identity..."
-if ! az login --identity --subscription "$AZURE_SUBSCRIPTION_ID"; then
-    echo "Failed to login with managed identity"
+if [[ -n "${AZURE_CLIENT_ID:-}" ]]; then
+  if ! az login --identity --username "$AZURE_CLIENT_ID"; then
+    echo "1 Failed to login with managed identity"
     exit 1
+  fi
+else
+  if ! az login --identity; then
+    echo "2 Failed to login with managed identity"
+    exit 1
+  fi
+fi
+
+# Optionally set the subscription if provided
+if [[ -n "${AZURE_SUBSCRIPTION_ID:-}" ]]; then
+  echo "Setting subscription..."
+  if ! az account set --subscription "$AZURE_SUBSCRIPTION_ID"; then
+    echo "Failed to set subscription"
+    exit 1
+  fi
 fi
 
 # Verify login by listing subscriptions
 echo "Verifying login by listing subscriptions..."
 if ! az account list --query "[].name" -o tsv; then
-    echo "Failed to list subscriptions"
-    exit 1
+  echo "Failed to list subscriptions"
+  exit 1
 fi
 
 echo "Login successful and verified"
