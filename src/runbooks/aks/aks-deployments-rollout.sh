9ebe61bccbc136d839f66febc12fb5413275a881
Fabio Felici <fabio.felici@pagopa.it>
2025-09-29 17:17:46 +0200

    Log parameter count to stderr in `aks-deployments-rollout.sh` for error visibility.

--- src/runbooks/aks/aks-deployments-rollout.sh
+++ src/runbooks/aks/aks-deployments-rollout.sh
@@ -7,11 +7,10 @@
 # - Namespace
 # - Deployment name
 
-echo $#
-
 # Check if required parameters are provided
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <namespace> <deployment-name>"
+    >&2 echo "error: $#"
     exit 1
 fi
 
