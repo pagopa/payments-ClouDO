7146f77f3e96635ff92b8fa1b3a1b9573d0dd030
Fabio Felici <fabio.felici@pagopa.it>
2025-10-08 14:48:49 +0200

    Fix `identity.tf` to reference resource group name directly from `azurerm_user_assigned_identity`.

--- src/core/iac/identity.tf
+++ src/core/iac/identity.tf
@@ -3,7 +3,7 @@ module "workload_identity_init" {
   source   = "git::https://github.com/pagopa/terraform-azurerm-v4.git//kubernetes_workload_identity_init?ref=8265f125b07251a5efe7b9ff57707109de8b46ba"
 
   workload_identity_name                = azurerm_user_assigned_identity.identity.name
-  workload_identity_resource_group_name = var.resource_group_name
+  workload_identity_resource_group_name = azurerm_user_assigned_identity.identity.resource_group_name
   workload_identity_location            = var.location
   workload_identity_name_prefix         = var.prefix
 }
@@ -14,7 +14,7 @@ module "workload_identity_config" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v4.git//kubernetes_workload_identity_configuration?ref=8265f125b07251a5efe7b9ff57707109de8b46ba"
 
   workload_identity_name                = azurerm_user_assigned_identity.identity.name
-  workload_identity_resource_group_name = var.resource_group_name
+  workload_identity_resource_group_name = azurerm_user_assigned_identity.identity.resource_group_name
   aks_name                              = each.value.name
   aks_resource_group_name               = each.value.resource_group
   namespace                             = "*"
