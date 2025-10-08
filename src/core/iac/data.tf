984f9a059a922fd79440b91271810698f1c5e20d
Fabio Felici <fabio.felici@pagopa.it>
2025-10-08 16:44:44 +0200

    Remove unused workload identity configuration module and commented out Key Vault data source.

--- src/core/iac/data.tf
+++ src/core/iac/data.tf
@@ -1,8 +1,8 @@
 # Key vault
-data "azurerm_key_vault" "kv" {
-  name                = var.key_vault.name
-  resource_group_name = var.key_vault.resource_group
-}
+# data "azurerm_key_vault" "kv" {
+#   name                = var.key_vault.name
+#   resource_group_name = var.key_vault.resource_group
+# }
 
 # App Insights
 data "azurerm_application_insights" "this" {
--- src/core/iac/identity.tf
+++ src/core/iac/identity.tf
@@ -28,20 +28,3 @@ module "workload_identity_init" {
   workload_identity_location            = var.location
   workload_identity_name_prefix         = var.prefix
 }
-
-module "workload_identity_config" {
-  for_each = var.aks_integration
-
-  source = "git::https://github.com/pagopa/terraform-azurerm-v4.git//kubernetes_workload_identity_configuration?ref=8265f125b07251a5efe7b9ff57707109de8b46ba"
-
-  workload_identity_name                = azurerm_user_assigned_identity.identity[each.key].name
-  workload_identity_resource_group_name = azurerm_user_assigned_identity.identity[each.key].resource_group_name
-  aks_name                              = each.value.name
-  aks_resource_group_name               = each.value.resource_group
-  namespace                             = "*"
-
-  key_vault_id                      = data.azurerm_key_vault.kv.id
-  key_vault_certificate_permissions = ["Get"]
-  key_vault_key_permissions         = ["Get"]
-  key_vault_secret_permissions      = ["Get"]
-}
