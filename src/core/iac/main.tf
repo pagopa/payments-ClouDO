0dfd2c495eb3658c9ed2824d471addd502dabc39
Fabio Felici <fabio.felici@pagopa.it>
2025-10-08 16:35:40 +0200

    Move identity resources from `main.tf` to `identity.tf` for improved organization.

--- src/core/iac/main.tf
+++ src/core/iac/main.tf
@@ -197,24 +197,3 @@ resource "azurerm_storage_table_entity" "schemas" {
     }
   )
 }
-
-
-# Identity
-resource "azurerm_user_assigned_identity" "identity" {
-  for_each            = var.aks_integration
-  location            = each.value.location
-  name                = "${var.prefix}-${each.value.location}-cloudo-identity"
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_role_assignment" "role_assignment" {
-  for_each = merge(
-    { for aks_key, _ in var.aks_integration : "${aks_key}:Contributor" => { role = "Contributor", key = aks_key } },
-    { for aks_key, _ in var.aks_integration : "${aks_key}:AKSClusterUser" => { role = "Azure Kubernetes Service Cluster User Role", key = aks_key } },
-    { for aks_key, _ in var.aks_integration : "${aks_key}:AKSRBACWriter" => { role = "Azure Kubernetes Service RBAC Writer", key = aks_key } }
-  )
-
-  scope                = data.azurerm_subscription.current.id
-  role_definition_name = each.value.role
-  principal_id         = azurerm_user_assigned_identity.identity[each.value.key].principal_id
-}
