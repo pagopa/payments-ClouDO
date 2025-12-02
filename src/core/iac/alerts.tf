e0c224a9d670db7f6f03bfb3e642c9af37007290
ffppa <fabio.felici@pagopa.it>
2025-12-02 10:54:37 +0100

    feat: [PAYMCLOUD-572] Add Azure Monitor alerts and improve alert configuration (#48)
    ### Description of Changes
    
    This pull request introduces enhanced Azure Monitor metrics and alerts
    to improve monitoring capabilities. Key updates include:
    
    - **New Features:**  
    - Added window size and frequency settings to Azure Monitor metric
    alerts for both queue and dead letter queue thresholds.
    - Implemented Azure Monitor alerts for function failures, execution
    durations, queue message counts, and dead letter queue thresholds.
    
    - **Refactoring:**  
    - Adjusted Azure Monitor alert configurations to support namespace
    updates, use dynamic dimensions, and improve threshold settings.
    
    ### Checklist
    
    - [x] Unit tests have been added/updated where applicable.
    - [x] Documentation has been updated to reflect the changes, if
    necessary.
    - [x] Relevant dependencies have been updated.  
    - [ ] Changes have been tested and verified.  
    
    ### Additional Information
    
    These changes aim to provide more granular monitoring and ensure timely
    alerting for various parameters in the Azure infrastructure.

---
+++ src/core/iac/alerts.tf
@@ -0,0 +1,64 @@
+resource "azurerm_monitor_metric_alert" "function_failures" {
+  for_each = merge(
+    { (azurerm_linux_function_app.orchestrator.name) = azurerm_linux_function_app.orchestrator.id },
+    { for w in azurerm_linux_function_app.worker : w.name => w.id }
+  )
+
+  name                = "${var.prefix}-${var.env}-cloudo-func-fail-${each.key}"
+  resource_group_name = var.resource_group_name
+  scopes              = [each.value]
+  description         = "Alert when function execution failures occur"
+  severity            = 2
+
+  criteria {
+    metric_namespace = "Microsoft.Web/sites"
+    metric_name      = "Http5xx"
+    aggregation      = "Total"
+    operator         = "GreaterThan"
+    threshold        = 0
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "function_duration" {
+  for_each = merge(
+    { (azurerm_linux_function_app.orchestrator.name) = azurerm_linux_function_app.orchestrator.id },
+    { for w in azurerm_linux_function_app.worker : w.name => w.id }
+  )
+
+  name                = "${var.prefix}-${var.env}-cloudo-func-duration-${each.key}"
+  resource_group_name = var.resource_group_name
+  scopes              = [each.value]
+  description         = "Alert when function execution duration exceeds threshold"
+  severity            = 2
+
+  criteria {
+    metric_namespace       = "Microsoft.Web/sites"
+    metric_name            = "AverageResponseTime"
+    aggregation            = "Average"
+    operator               = "GreaterThan"
+    threshold              = 1000
+    skip_metric_validation = true
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "queue_message_count" {
+  name                = "${var.prefix}-${var.env}-cloudo-queue-msg-count"
+  resource_group_name = var.resource_group_name
+
+  scopes = ["${module.storage_account.id}/queueServices/default"]
+
+  description = "Alert when queue message count exceeds threshold"
+  severity    = 2
+
+  window_size = "PT1H"
+  frequency   = "PT1H"
+
+  criteria {
+    metric_namespace       = "Microsoft.Storage/storageAccounts/queueServices"
+    metric_name            = "QueueMessageCount"
+    aggregation            = "Average"
+    operator               = "GreaterThan"
+    threshold              = 500
+    skip_metric_validation = true
+  }
+}
