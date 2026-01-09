3927cec04ddb58144ab2d859d3f4b860fc6a93db
ffppa <fabio.felici@pagopa.it>
2026-01-09 16:08:54 +0100

    fix: Correct null check for `vnet_name` in `cloudo_flexible_snet` module

--- src/core/iac/networks.tf
+++ src/core/iac/networks.tf
@@ -1,7 +1,7 @@
 
 # Subnet
 module "cloudo_flexible_snet" {
-  count                = var.vnet_name ? 1 : 0
+  count                = var.vnet_name != null ? 1 : 0
   source               = "git::https://github.com/pagopa/terraform-azurerm-v4//IDH/subnet?ref=e37760466d614653cd385450ec9458aae1055428"
   name                 = "${var.prefix}-snet"
   resource_group_name  = var.resource_group_name
